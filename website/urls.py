4b2b7588b9b8b8dddbe1989da9b1939a0ae7464e
Студент ПТК <student@ptk.ru>
2024-01-29 10:09:20 +0300

    more btn-s

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from website.views import *
 urlpatterns = [
     path('',index),
     path('category/<int:id>', index),
+    path('news/<int:id>', news_page),
 ]
--- website/views.py
+++ website/views.py
@@ -9,4 +9,10 @@ def index(request, id=None):
     if id:
         data["news"] = News.objects.filter(category=id)
     return render(request, "index.html",data)
+
+def news_page(request, id):
+    data = {
+        "news":News:objects.get(id=id)
+    }
+    return render(request, "news_page.html",data)
     
